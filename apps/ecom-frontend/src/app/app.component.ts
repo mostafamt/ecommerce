b7d571565c1f1b8e225dea2fc7d645493482f1f1
mostafa <mostafa.kamel@almotahidaeducation.com>
2024-11-26 16:25:36 +0100

    add font-awesome

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit {
+  private faConfig = inject(FaConfig);
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    console.log('initFontAwesome');
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
