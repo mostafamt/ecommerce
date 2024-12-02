6c2b0f5d6ee34cc300765911e33131549d922b46
mostafa <mostafa.kamel@almotahidaeducation.com>
2024-12-02 17:26:22 +0100

    add nxrocks

--- apps/ecom-frontend/src/app/layout/footer/footer.component.ts
+++ apps/ecom-frontend/src/app/layout/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'ecom-footer',
   standalone: true,
   imports: [CommonModule, FaIconComponent],
