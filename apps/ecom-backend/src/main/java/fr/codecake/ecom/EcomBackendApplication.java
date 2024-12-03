aaac3a0e46096b9af009b4cec0c44e5354188656
mostafa <mostafa.kamel@almotahidaeducation.com>
2024-12-03 09:28:42 +0100

    edit backend generation

--- apps/ecom-backend/src/main/java/fr/codecake/ecom/EcomBackendApplication.java
+++ apps/ecom-backend/src/main/java/fr/codecake/ecom/EcomBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestappApplication {
+public class EcomBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestappApplication.class, args);
+		SpringApplication.run(EcomBackendApplication.class, args);
 	}
 
 }
